04c3e6c1d9542f686fa7fbe4f51860996ab336ba
jon4444 <50190069+jon4444@users.noreply.github.com>
2021-09-17 14:28:40 +0100

    first commit containing the department codes

---
+++ emailApp/out/production/emailApp/ReadMe.txt
@@ -0,0 +1,8 @@
+You are an IT Support Administrator Specialist and are charged with the task of creating email accounts for new hires.
+
+Your application should do the following:
+- Generate an email with the following syntax
+- Determine the department (sales, development, accounting), if none leave blank
+- Generate a random string for a password
+- Have set methods to change the password
+- Have get methods to display the name, email and mailbox capacity
---
+++ emailApp/src/Email.java
@@ -0,0 +1,44 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.util.Scanner;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    // constructor to receive the firstname and lastname
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName );
+
+        // call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
+    }
+
+    // Ask for the department
+    private String setDepartment() {
+        System.out.println("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter the department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if(depChoice == 1) { return "sales";}
+        else if(depChoice == 2) { return "development"; }
+        else if(depChoice == 3) { return "Accounting"; }
+        else { return ""; }
+    }
+
+    // Generate a random password
+
+    // Set the mailbox capacity
+
+    // set the alternate email
+
+    // change the password
+
+}
